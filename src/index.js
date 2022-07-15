b99e7e8da1fed3952789d56ffe7c756d7aae20eb
Akshay Neje <98444126+akshayneje2823@users.noreply.github.com>
2022-07-15 20:35:54 +0530

    Javascript writing javascript in JSX

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Akshay ";
+const lName = "Neje";
+
+const num = 99;
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite fruits</h1>
-    <ul>
-      <li>Mango</li>
-      <li>Banana</li>
-      <li>Apple</li>
-    </ul>
+    <h1>My Name Is {fName + lName}</h1>
+    <p>Favourate number is {num}</p>
   </div>,
   document.getElementById("root")
 );
