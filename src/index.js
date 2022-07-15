31afbe171b02b461c67bef0088f262f950dcbbd8
Akshay Neje <98444126+akshayneje2823@users.noreply.github.com>
2022-07-15 20:16:18 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite fruits</h1>
+    <ul>
+      <li>Mango</li>
+      <li>Banana</li>
+      <li>Apple</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
